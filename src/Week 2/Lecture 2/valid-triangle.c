1ca18c37f00e0ca38fb7ba6e2c50031788309506
Brandon Jiagge <bejiagge@gmail.com>
2024-01-29 19:37:27 +0000

    Update valid-triangle.c

--- src/Week 2/Lecture 2/valid-triangle.c
+++ src/Week 2/Lecture 2/valid-triangle.c
@@ -6,27 +6,32 @@ bool triangle(int a, int b, int c);
 
 int main(int argc, char* argv[])
 {
+    int arg1 = atoi(argv[1]);
+    int arg2 = atoi(argv[2]);
+    int arg3 = atoi(argv[3]);
+
+
     if (argc != 4)
     {
         printf("Invalid command line argument. Must input 3 positive integers to calculate.\n");
         return 1;
     }
-    else if (atoi(argv[1]) < 1 || atoi(argv[2]) < 1 || atoi(argv[3]) < 1)
+    else if (arg1 < 1 || arg2 < 1 || arg3 < 1)
     {
         printf("Invalid command line argument. Must input 3 positive integers.\n");
         return 1;
     }
     else
     {
-        bool n = triangle(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+        bool n = triangle(arg1, arg2, arg3);
         if (!n)
         {
-            printf("A triangle with lengths of %d, %d, and %d is possible.\n", atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+            printf("A triangle with lengths of %d, %d, and %d is possible.\n", arg1, arg2, arg3);
             return 0;
         }
         else
         {
-            printf("A triangle with lengths of %d, %d, and %d is not possible.\n", atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+            printf("A triangle with lengths of %d, %d, and %d is not possible.\n", arg1, arg2, arg3);
             return 0;
         }
     }
